2017ceafc9ed1f640b5d845d04e7c67dcc6911cc
Yasin-Rohani <yasinrohani89@gmail.com>
2024-11-27 09:15:45 +0100

    First commit

---
+++ Bulky2/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Bulky2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky2.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+        base(options)  
+    {
+        
+    }
+
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+}
---
+++ Bulky2/Views/Category/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bulky2.Views.Category;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
