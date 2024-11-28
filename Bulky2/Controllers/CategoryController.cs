e504e817be8cf771688186f59f2c8e56bfbe9b07
Yasin-Rohani <yasinrohani89@gmail.com>
2024-11-28 11:53:06 +0100

    BootsWatch

--- Bulky2/Controllers/CategoryController.cs
+++ Bulky2/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bulky2.Data;
+using Bulky2.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bulky2.Controllers;
 
 public class CategoryController : Controller
 {
     // GET
+
+   private readonly ApplicationDbContext _db;
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
     public IActionResult Index()
     {
-        return View();
+        List<Category>objCategoryList= _db.Categories.ToList();   
+        return View(objCategoryList);
     }
 }
